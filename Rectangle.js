ca8195d7159309de407f9bf38c58af978d057acc
walidnemri <nemriwalid@hotmail.com>
2020-09-24 16:59:04 +0200

    git tag step-3.

---
+++ Rectangle.js
@@ -0,0 +1,25 @@
+class Rectangle {
+    constructor(a,b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    isSquare() {
+        
+       if ( this.a === this.b ) return true;
+
+       return false;
+    }
+
+    getArea() {
+        return this.a * this.b;
+
+    }
+
+    getPerimeter() {
+        return (this.a + this.b)*2;
+
+    }
+}
+
+module.exports = Rectangle;
